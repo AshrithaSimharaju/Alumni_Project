412327b32e8d45199f0c11fe895e044e1f3ee4d9
AshrithaSimharaju <163991546+AshrithaSimharaju@users.noreply.github.com>
2025-02-10 13:33:36 +0000

    Update Contact.js

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -1,33 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const ContactSchema = new mongoose.Schema({
-//   sender: {
-//     type: String,
-//     required: true,
-//   },
-//   receiver: {
-//     type: String,
-//     required: true,
-//   },
-//   subject: {
-//     type: String,
-//     required: true,
-//     enum: ["general", "event", "donation"], // Restrict to valid subjects
-//   },
-//   message: {
-//     type: String,
-//     required: true,
-//   },
-//   timestamp: {
-//     type: Date,
-//     default: Date.now,
-//   },
-// });
-// const Contact = mongoose.model('Contact', ContactSchema);
-
-// // Export the Contact model as default
-// export default Contact;
-
 
 import mongoose from 'mongoose';
 
